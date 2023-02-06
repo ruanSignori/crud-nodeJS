56de2364902f43a54cd57efb68f2dc2b11b68e6e
ruanSignori <cleosignori@gmail.com>
2023-02-06 12:27:08 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Opa')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Olá mundo')
+})
+
+app.listen(port, () => console.log(`Server is running at localhost:${port}.`))
