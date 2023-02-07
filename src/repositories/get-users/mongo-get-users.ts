ed78d4ceff7cffc6380201f601e903976f2754dc
ruanSignori <cleosignori@gmail.com>
2023-02-07 07:32:26 -0300

    garantir GetUsersMongoRepository de pegar os users do mongoDB

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -4,8 +4,14 @@ import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    const users = await MongoClient.db.collection<User>("users").find({}).toArray();
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
 
-    return [];
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
